3a87e17c7202f9619223cb2f40238a92a83650b9
Joao Andrade <joao.andrade@cgi.com>
2020-01-03 16:44:21 +0000

    jaden case kata fixes

--- src/pt/and/codewars/JadenCase.java
+++ src/pt/and/codewars/JadenCase.java
@@ -5,14 +5,11 @@ public class JadenCase {
 	public static String toJadenCase(String phrase) {
 		// TODO put your code below this comment
 		String result = "";
-		if (phrase == null)
+		if (phrase == null || phrase == "")
 			result = null;
-		else if (phrase == "") {
-			result = "";
-		}
 		else {
 			for (String word : phrase.split(" ")) {
-				result += phrase.replace(word, word.substring(0, 1).toUpperCase() + word.substring(1)) + " ";
+				result += word.replace(word, word.substring(0, 1).toUpperCase() + word.substring(1)) + " ";
 			}
 			result = result.substring(0, result.length() -1);
 		}
