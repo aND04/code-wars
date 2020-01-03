07d11ed0a72d83776848b0ce36510304fd357dd3
Joao Andrade <joao.andrade@cgi.com>
2020-01-03 13:41:59 +0000

    bit counting kata

---
+++ src/pt/and/codewars/BitCounting.java
@@ -0,0 +1,19 @@
+package pt.and.codewars;
+
+public class BitCounting {
+
+	public static int countBits(int n){
+		// Show me the code!
+		String binary = "";
+		while (n != 0) {
+			binary += (n % 2 == 0) ? "0" : "1";
+			n = n / 2;
+		}
+		return binary.length() - binary.replace("1", "").length();
+	}
+
+	public static void main(String[] args) {
+		countBits(1234);
+	}
+
+}
